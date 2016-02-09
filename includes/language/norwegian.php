03b425c2840e39c55127359cba5828cafa41b66e
Helge Sverre <helge.sverre@gmail.com>
2016-02-09 18:09:36 +0100

    Added Norwegian Translation

---
+++ includes/language/norwegian.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * @package HelpDeskZ
+ * @website: http://www.helpdeskz.com
+ * @community: http://community.helpdeskz.com
+ * @author Evolution Script S.A.C.
+ * @since 1.0.0
+ */
+$LANG['HOME'] = 'Hjem';
+$LANG['MY_TICKETS'] = 'Mine Saker';
+$LANG['SUBMIT_A_TICKET'] = 'Lag ny sak';
+$LANG['KNOWLEDGEBASE'] = 'Kunnskapsbase';
+$LANG['NEWS'] = 'Nyheter';
+$LANG['ACCOUNT_LOGIN'] = 'Konto Innlogging';
+$LANG['LASTEST_UPDATES'] = 'Siste oppdateringer';
+$LANG['YOUR_EMAIL_ADDRESS'] = 'Epost addressen din';
+$LANG['YOUR_PASSWORD'] = 'Passordet ditt';
+$LANG['REMEMBER_ME'] = 'Husk meg';
+$LANG['LOST_PASSWORD'] = 'Glemt passord';
+$LANG['LOGIN'] = 'Logg inn';
+$LANG['ACCOUNT'] = 'Konto';
+$LANG['MY_PROFILE'] = 'Min profil';
+$LANG['PREFERENCES'] = 'Preferanser';
+$LANG['CHANGE_PASSWORD'] = 'Bytt passord';
+$LANG['LOGOUT'] = 'Logg ut';
+$LANG['VIEW_ALL_NEWS'] = 'Vis alle nyheter';
+$LANG['POSTED_BY_ON'] = 'Publisert av %author% den %date%';
+$LANG['OLDEST_POSTS'] = 'Eldste publikasjoner';
+$LANG['NEWEST_POSTS'] = 'Nyeste publikasjoner';
+$LANG['MOST_POPULAR_ARTICLES'] = 'Mest populære artikkler';
+$LANG['NEWEST_ARTICLES'] = 'Nyeste artikkler';
+$LANG['MORE_TOPICS'] = 'Flere emner';
+$LANG['FILE_NOT_FOUND'] = 'Fant ikkje filen.';
+$LANG['SEARCH'] = 'Søk';
+$LANG['SEARCH_IN_KNOWLEDGEBASE'] = 'Søk i Kunnskapsbasen';
+$LANG['SEARCH_RESULTS'] = 'Søkeresultater';
+$LANG['SEARCH_NOT_FOUND'] = 'Vi fant ikkje noen relevante resultater for søket ditt.';
+$LANG['ENTER_EMAIL_RECOVER_PASSWORD'] = 'Skriv inn eposten din og trykk på knappen, Du vil etter kort tid få ein epost med ein link som vil resette passordet ditt.';
+$LANG['ACCOUNT_INFORMATION'] = 'Kontoinformasjon';
+$LANG['EMAIL'] = 'E-post';
+$LANG['CAPTCHA_VERIFICATION'] = 'CAPTCHA Verifikasjon';
+$LANG['ENTER_TEXT_CAPTCHA'] = 'Skriv inn teksten du ser på bildet nedenfor inn i tekstboksen, dette er for å forhindre søppelmeldinger.';
+$LANG['SUBMIT'] = 'Send';
+
+
+$LANG['CANT_FIND_SOLUTION_SUBMIT_TICKET'] = 'If you can\'t find a solution to your problem, you can submit a ticket by selecting the appropriate department below.';
+$LANG['SELECT_DEPARTMENT_TICKET'] = 'Velg avdelingen saken din skal sendes til.';
+$LANG['CSRF_ERROR'] = 'Det oppstod eit problem (Cross-site request forgery detected); Vennligst prøv igjen.';
+$LANG['DEPARTMENTS'] = 'Avdelinger';
+$LANG['NEXT'] = 'Neste';
+$LANG['INVALID_EMAIL_OR_PASSWORD'] = 'Ugyldig E-post eller passord.';
+$LANG['YOUR_TICKET_DETAILS'] = 'Dine saksdetaljer';
+$LANG['ENTER_TICKET_DETAILS'] = 'Skriv inn saksdetaljene dine nedenfor, hvis du rapporterer eit problem, vennligst husk å gi oss så mye relevant informasjon som mulig.';
+$LANG['GENERAL_INFORMATION'] = 'Generell Informasjon';
+$LANG['FULL_NAME'] = 'Fult navn';
+$LANG['PRIORITY'] = 'Prioritet';
+$LANG['YOUR_MESSAGE'] = 'Din Beskjed';
+$LANG['SUBJECT'] = 'Tittel';
+$LANG['ATTACHMENTS'] = 'Vedlegg';
+$LANG['INVALID_CAPTCHA_CODE'] = 'FEIL: Feil kode på bildeverifikasjonen';
+$LANG['INVALID_EMAIL_ADDRESS'] = 'FEIL: Vennligst benytt ein gyldig E-postadresse';
+$LANG['ONE_REQUIRED_FIELD_EMPTY'] = 'En av de obligatoriske feltenne er ikkje utfylt';
+$LANG['ERROR_UPLOADING_A_FILE'] = 'Det oppstod ein feil ved opplasting av ein fil';
+$LANG['INVALID_FILE_EXTENSION'] = 'Ugyldig filformat';
+$LANG['FILE_NOT_ALLOWED'] = 'Filen er ikkje tillatt.';
+$LANG['FILE_IS_BIG'] = 'Filen er for stor. Maks filstørrelse er %size%';
+$LANG['REQUEST_RECEIVED'] = 'Forespørselen din er mottatt';
+$LANG['REQUEST_RECEIVED_RESPONSE'] = 'Vi har mottatt din forespørsel og vi vil komme tilbake til deg snart. Du kan logge deg inn på helpdesken for å sjekke statusen for saken, eller sjekke e-posten for videre oppdateringer.';
+$LANG['TICKET_ID'] = 'Saksnummer';
+
+$LANG['WE_SENT_YOUR_NEW_PASSWORD'] = 'Vi har sendt en e-post med et nytt passord til adressen';
+$LANG['EMAIL_WAS_NOT_FOUND'] = 'E-post ble ikke funnet.';
+$LANG['SEARCH_TICKET_ID'] = 'Søk Saksnummer';
+$LANG['VIEW_TICKETS'] = 'Vis Saker';
+$LANG['VIEW_TICKETS_DESCRIPTION'] = 'Nedenfor er ein liste over saker du har sendt inn, Klikk på sakstittelen for å lese saksdetaljer og historikk.';
+$LANG['LAST_UPDATE'] = 'Sist oppdatert';
+$LANG['DEPARTMENT'] = 'Avdeling';
+$LANG['STATUS'] = 'Status';
+$LANG['VIEW_TICKET_ID'] = 'Vis Sak #%id%';
+$LANG['CREATED'] = 'Opprettet';
+$LANG['UPDATED'] = 'Oppdatert';
+$LANG['TICKET_IS_CLOSED'] = 'Saken er for nå lukket.';
+$LANG['ADD_REPLY'] = 'Legg til svar';
+$LANG['YOUR_REPLY_SENT'] = 'Svaret ditt for denne saken er sendt inn';
+$LANG['USER'] = 'Bruker';
+$LANG['STAFF'] = 'Personale';
+$LANG['POSTED_ON'] = 'publisert den';
+$LANG['TICKET_NOT_FOUND_OR_PERMISSION'] = 'Beklager, vi kunne ikkje finne saken du så etter, eller du har ikkje rettigheter til å se den.';
+$LANG['TICKET_NOT_FOUND'] = 'Sak ikkje funnet.';
+$LANG['YOUR_PROFILE_UPDATED'] = 'Profilen din er blitt oppdatert.';
+$LANG['UPDATE'] = 'Oppdater';
+$LANG['YOUR_PREFERENCES_SAVED'] = 'Innstillingene dine er blitt lagret.';
+$LANG['GENERAL_OPTIONS'] = 'Generelle Innstillinger';
+$LANG['TIMEZONE'] = 'Tidsone';
+$LANG['DEFAULT_TIMEZONE'] = 'Standard Tidsone';
+$LANG['PASSWORD_UPDATED'] = 'Passord oppdatert.';
+$LANG['PASSWORD_DETAILS'] = 'Passord Detaljer';
+$LANG['EXISTING_PASSWORD'] = 'Eksisterende Passord';
+$LANG['NEW_PASSWORD'] = 'Nytt Passord';
+$LANG['CONFIRM'] = 'Bekreft';
+$LANG['EXISTING_PASSWORD_INCORRECT'] = 'Ditt nåværende passord er feil.';
+$LANG['NEW_PASSWORDS_DO_NOT_MATCH'] = 'Di nye passordene samsvarte ikke.';
+$LANG['EMAIL_ASSOCIATED_OTHER_ACCOUNT'] = 'E-postadressen er allerede knyttet til en annen konto.Email address is already associated with another account.';
+$LANG['WE_ARE_PERFORMING_MAINTENANCE'] = 'Vi holder for øyeblikket på med planlagt vedlikehold, Vi beklager eventuelle problemer dette medfører.';
+$LANG['MAINTENANCE_MODE'] = 'Vedlikeholdsmodus';
+$LANG['PAGE_NOT_FOUND'] = 'Side ikkje funnet - 404';
+$LANG['CLICK_TO_BACK_HELPDESK'] = 'Klikk her for å gå tilbake til hovedsiden.';
+$LANG['OPEN'] = 'Åpen';
+$LANG['AWAITING_REPLY'] = 'Venter på svar';
+$LANG['IN_PROGRESS'] = 'Under Behandling';
+$LANG['ANSWERED'] = 'Besvart';
+$LANG['CLOSED'] = 'Lukket';
+?>
