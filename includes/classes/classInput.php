4eff28ebb6292a97601642c46732f626e051dbb6
Travis Newton <travis.newton@nodespace.net>
2023-03-02 00:01:16 -0500

    Updated input santitation class

--- includes/classes/classInput.php
+++ includes/classes/classInput.php
@@ -1,71 +1,46 @@
 <?php
+// TODO: Update copyright on this code
 /**
- * @package HelpDeskZ
- * @website: http://www.helpdeskz.com
- * @community: http://community.helpdeskz.com
- * @author Evolution Script S.A.C.
- * @since 1.0.0
+ * Input Cleaner
+ *
+ * @package Xinsto Desk
+ * @version 2.0.0
+ * @link 
+ * @license GPLv2
  */
-class Input_Cleaner
-{
-	var $cleaned_vars = array();
-    public function __construct()
-	{
-		$this->test = 'asas';
-		if(function_exists('get_magic_quotes_runtime') && get_magic_quotes_runtime())
-			set_magic_quotes_runtime(false);
-			
-			if(get_magic_quotes_gpc()) {
-				$this->array_stripslashes($_POST);
-				$this->array_stripslashes($_GET);
-				$this->array_stripslashes($_COOKIES);
-			}
-			$this->frm = $_POST;
-			$this->frmg = $_GET;
-			$this->cookie = $_COOKIE;
-			while (list ($kk, $vv) = each ($this->frm)){
-				if (is_array ($vv)){
-					$vv_cleaned = $vv;
-				}else{
-				  $vv = trim ($vv);
-				  $vv_cleaned = htmlspecialchars(trim($vv));
-				}
-				$this->p[$kk] = $vv;
-				$this->pc[$kk] = $vv_cleaned;
-				$this->r[$kk] = $vv;
-				$this->rc[$kk] = $vv_cleaned;
-			}
-			while (list ($kk, $vv) = each ($this->frmg)){
-				if (is_array ($vv)){
-					$vv_cleaned = $vv;
-				}else{
-				  $vv = trim ($vv);
-				  $vv_cleaned = htmlspecialchars(trim($vv));
-				}
-				$this->g[$kk] = $vv;
-				$this->gc[$kk] = $vv_cleaned;
-				$this->r[$kk] = $vv;
-				$this->rc[$kk] = $vv_cleaned;
-			}
-			while (list ($kk, $vv) = each ($this->cookie)){
-				if (is_array ($vv)){
-				}else{
-				  $vv = trim ($vv);
-				  $vv_cleaned = htmlspecialchars(trim($vv));
-				}
-				$this->c[$kk] = $vv;
-				$this->cc[$kk] = $vv_cleaned;
-			}
-			
-	}
-	
-	function array_stripslashes(&$array) {
-		if(is_array($array))
-			while(list($key) = each($array))
-				if(is_array($array[$key]))
-					$this->array_stripslashes($array[$key]);
-				else
-					$array[$key] = stripslashes($array[$key]);
-	}
+
+ /**
+  * @class Input_Cleaner
+  */
+class Input_Cleaner {
+	/**
+	 * @var array|string
+	 */
+    public function sanitize($input) {
+        if (is_array($input)) {
+            foreach ($input as &$value) {
+                $value = $this->sanitize($value);
+            }
+            unset($value); // unset reference to the last element
+        } else {
+            $input = $this->array_stripslashes($input);
+            $input = trim($input);
+            $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
+        }
+        return $input;
+    }
+
+    private function array_stripslashes(&$input) {
+        if (is_array($input)) {
+            foreach ($input as &$value) {
+                $this->array_stripslashes($value);
+            }
+            unset($value); // unset reference to the last element
+        } else {
+            $input = stripslashes($input);
+        }
+        return $input;
+    }
 }
+
 ?>
